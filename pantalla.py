d4497f1e7ba609359d84e7c3c6e72aa0434a0a5c
unknown <dmitos@gmail.com>
2018-03-22 22:55:22 -0300

    se agregaron comentarios a la clase titulo

--- pantalla.py
+++ pantalla.py
@@ -37,18 +37,27 @@ class Pantalla:
     def titulo(texto, col=1, fila=1, linea="─", estilo='0;', color='37',
                fondo='m'):
         # ver la solucion para que el texsto siempre tenga lineas arriba
-        texto = texto.upper()
-        SizeP = DimPan.ObtTamTer()
+        texto = texto.upper()  # texto en mayuscula
+        SizeP = DimPan.ObtTamTer()  # obtengo dimensiones del modulo DimPan
         SizeCol, SizeFil = SizeP[0][0], SizeP[0][1]
-        largo = SizeCol - col * 2
-        # largot = largo - col * 2
+        largo = SizeCol - col * 2  # largo de la linea sobre/debajo del texto
         LarTex = len(texto)
         centrar = int((SizeCol - LarTex) / 2)
+        '''
+        ---------------------------------------
+        esto es por si colocan valores en la
+        columna o fila que ocasionen problemas
+        al momento de colocar en pantalla
+        '''
         if fila < 1 or fila > (SizeFil - 2):
             print(SizeFil - 3)
             fila = 1
         if col < 1:
             col = 1
+        '''
+        ---------------------------------------
+        determino como se muestra lo solicitado en pantalla
+        '''
         print(Pantalla.pos(col, fila), linea * largo)
         print(Pantalla.pos(centrar, fila + 1),
               str(Color.f) + estilo + color + fondo + texto + '\033[0;m')
